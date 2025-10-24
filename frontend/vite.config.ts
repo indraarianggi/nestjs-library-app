c9547734ec197a1a920e1125919adb7028300d96
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2025-10-24 09:32:17 +0700

    chore(workflows): update deployment source paths and Vite configuration
    - Modified backend and frontend deployment workflows to adjust file source paths for better clarity and consistency.
    - Updated Vite configuration to set the base path dynamically based on the environment mode, enhancing deployment flexibility.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/library/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -19,4 +20,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
